e1508a8f56d531ba0ed128bfc531345bf21f9967
Torsten Ernst <torsten@wostmann.com>
2020-12-04 15:01:38 -0500

    Build better deployment script.

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+mkdir ~/temp
+cd ~/temp
+git clone https://github.com/tors10e/terra.git
+cd terra
+git checkout master
+scp -r ./* torsten@terraincognitavineyard.com:/home/www/terraincognitavineyard.com
+rm ~/temp/terra
