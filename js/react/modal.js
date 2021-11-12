c75ecd5c6dc99d99ee8fb9e65496f8471a6ec42a
Torsten Ernst <torstenaernst@gmail.com>
2021-11-12 14:12:30 -0500

    Made cabernet modal use React JS and updated events to include nov 12-14.

--- js/react/modal.js
+++ js/react/modal.js
@@ -1,36 +1,39 @@
 'use strict';
 
-//import $ from 'jquery';
+function CabernetModal(props) {
+    return React.createElement(Modal, {
+        id: "cabernet-modal",
+        jquery_id: "#cabernet-modal",
+        image_source: "./images/1000x1000/cab_sav_1000x1000.jpg",
+        name: "Cabernet Sauvignon",
+        modal_text: "Notes of plum, black cherry and tobacco meld to create a medium body and lightly oaked red wine\r that's perfect for settling into a relaxing evening."
+    });
+}
 
 function Modal(props) {
     return React.createElement(
         "div",
-        { "class": "col-sm plus-block--item", onClick: ShowModal },
+        { "class": "plus-block--item-wrapper inline", onClick: ShowModal },
         React.createElement(
             "div",
-            { "class": "plus-block--item-wrapper inline" },
+            { "class": "plus-block--img-wrapper" },
+            React.createElement("img", { src: props.image_source }),
+            React.createElement(
+                "h3",
+                null,
+                props.name
+            ),
             React.createElement(
                 "div",
-                { "class": "plus-block--img-wrapper" },
-                React.createElement("img", { src: "./images/1000x1000/cab_sav_1000x1000.jpg" }),
-                React.createElement(
-                    "h3",
-                    null,
-                    "Cabernet Sauvignon"
-                ),
+                { "class": "modal fade", id: props.id, tabindex: "-1", role: "dialog", "aria-hidden": "true" },
                 React.createElement(
                     "div",
-                    { "class": "modal fade", id: "cabernet-modal", tabindex: "-1", role: "dialog",
-                        "aria-hidden": "true" },
+                    { "class": "modal-dialog", role: "document" },
                     React.createElement(
                         "div",
-                        { "class": "modal-dialog", role: "document" },
-                        React.createElement(
-                            "div",
-                            { "class": "modal-content" },
-                            React.createElement(ModalHeader, null),
-                            React.createElement(ModalBody, { text: "Notes of plum, black cherry and tobacco meld to create a\r medium body and lightly oaked red wine that's perfect for settling\r into a relaxing evening." })
-                        )
+                        { "class": "modal-content" },
+                        React.createElement(ModalHeader, { name: props.name }),
+                        React.createElement(ModalBody, { text: props.modal_text })
                     )
                 )
             )
@@ -45,7 +48,7 @@ function ModalHeader(props) {
         React.createElement(
             "h5",
             { "class": "modal-title" },
-            "Cabernet Sauvignon"
+            props.name
         ),
         React.createElement(
             "button",
@@ -80,5 +83,4 @@ function HideModal(props) {
 }
 
 var domContainer = document.querySelector('#modal_container');
-//const domContainer = document.getElementById('modal_container')
-ReactDOM.render(React.createElement(Modal, null), domContainer);
+ReactDOM.render(React.createElement(CabernetModal, null), domContainer);
--- src/modal.js
+++ src/modal.js
@@ -1,51 +1,50 @@
 'use strict';
 
 function CabernetModal(props) {
- data={image_source:"./images/1000x1000/cab_sav_1000x1000.jpg",
-    name:"Cabernet Sauvignon"}
-
-return (
-
-)
- }
-
+    return (
+    <Modal
+        id="cabernet-modal"
+        jquery_id="#cabernet-modal"
+        image_source="./images/1000x1000/cab_sav_1000x1000.jpg"
+        name="Cabernet Sauvignon"
+        modal_text="Notes of plum, black cherry and tobacco meld to create a medium body and lightly oaked red wine
+            that's perfect for settling into a relaxing evening."
+         />
+    );
+    }
 
 
 function Modal(props) {
    return(
-            <div class= onClick={ShowModal}  >
-                            <div class="plus-block--item-wrapper inline">
-                                <div class="plus-block--img-wrapper">
-                                    <img src={props.image_source}/>
-                                    <h3>Cabernet Sauvignon</h3>
-
-                                    <div class="modal fade" id="cabernet-modal" tabindex="-1" role="dialog"
-                                         aria-hidden="true">
-                                        <div class="modal-dialog" role="document">
-                                            <div class="modal-content">
-                                                <ModalHeader />
-                                                <ModalBody text="Notes of plum, black cherry and tobacco meld to create a
-                                                medium body and lightly oaked red wine that's perfect for settling
-                                                into a relaxing evening." />
-                                            </div>
-                                        </div>
+            <div class="plus-block--item-wrapper inline" onClick={ShowModal}  >
+                <div class="plus-block--img-wrapper">
+                    <img src={props.image_source}></img>
+                        <h3>{props.name}</h3>
+                            <div class="modal fade" id={props.id} tabindex="-1" role="dialog" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <ModalHeader name={props.name}/>
+                                            <ModalBody text={props.modal_text} />
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                </div>
+            </div>
   );
 }
 
+
 function ModalHeader(props) {
-return (
-<div class="modal-header">
-    <h5 class="modal-title">Cabernet Sauvignon</h5>
-        <button type="button" class="close" onClick={HideModal} aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-    </button>
-</div>
-);
+    return (
+        <div class="modal-header">
+            <h5 class="modal-title">{props.name}</h5>
+                <button type="button" class="close" onClick={HideModal} aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    );
 }
+
 
 function ModalBody(props) {
    return(
@@ -56,11 +55,13 @@ function ModalBody(props) {
    );
 }
 
+
 function ShowModal(props) {
     return(
         $('#cabernet-modal').modal('show')
     );
 }
+
 
 function HideModal(props) {
     return(
@@ -68,7 +69,8 @@ function HideModal(props) {
     );
 }
 
+
 const domContainer = document.querySelector('#modal_container');
 ReactDOM.render(
-    <Modal />, domContainer
+    <CabernetModal />, domContainer
 );
