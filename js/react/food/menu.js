9f6548fd323a44841f43b2585b603ae2f221f870
torsten <torstenaernst@gmail.com>
2022-03-25 10:16:32 -0400

    Updated food menu and changed component to use a table

--- js/react/food/menu.js
+++ js/react/food/menu.js
@@ -1,68 +1,120 @@
 
 function Menu(props) {
-                    return React.createElement(
-                                        "div",
-                                        null,
-                                        React.createElement(
-                                                            "h2",
-                                                            { "class": "text-decoration-underline" },
-                                                            "Wine Tasting Snacks Menu"
-                                        ),
-                                        React.createElement(
-                                                            "dl",
-                                                            { "class": "row" },
-                                                            React.createElement(
-                                                                                "dt",
-                                                                                { "class": "col-sm-3" },
-                                                                                "Garden Salad"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dd",
-                                                                                { "class": "col-sm-9 text-left" },
-                                                                                "Mixed greeens, tomato, cucumber, carrots, radishes, balsamic vinaigrette"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dt",
-                                                                                { "class": "col-sm-3" },
-                                                                                "Roasted Pepper Hummus"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dd",
-                                                                                { "class": "col-sm-9 text-left" },
-                                                                                "Served with pita chips and crackers"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dt",
-                                                                                { "class": "col-sm-3" },
-                                                                                "Chimichurri Asparagus"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dd",
-                                                                                { "class": "col-sm-9 text-left" },
-                                                                                "Carrots, parsley, shallot, and wine marinade"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dt",
-                                                                                { "class": "col-sm-3" },
-                                                                                "Charcuterie Box"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dd",
-                                                                                { "class": "col-sm-9 text-left" },
-                                                                                "Smoked gouda, colby jack, cured meats, pickled vegetables, hummus, pita chips or crackers"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dt",
-                                                                                { "class": "col-sm-3" },
-                                                                                "Chicken Salad Wrap"
-                                                            ),
-                                                            React.createElement(
-                                                                                "dd",
-                                                                                { "class": "col-sm-9 text-left" },
-                                                                                "Pecans, golden raisins, ginger, leaf lettuce, whole wheat wrap"
-                                                            )
-                                        )
-                    );
+    return React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h2",
+            { "class": "text-decoration-underline heading" },
+            "Wine Tasting Snacks Menu"
+        ),
+        React.createElement(
+            "table",
+            { "class": "table table-striped" },
+            React.createElement(
+                "tbody",
+                null,
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Feta Citrus Salad"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Mixed greens, blood orange, cucumber, goat feta cheese, sunflower seeds, citrus vinaigrette"
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Stuffed Grape Leaves"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "White rice, onion, mint"
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Roasted Garlic Hummus"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Served with pita chips and crackers"
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Chimichurri Asparagus"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Asparagus, carrots, red wine vinegar marinade"
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Charcuterie Box"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Smoked gouda, colby jack, cured meats, pickled vegetables, hummus, pita chips or crackers"
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Chicken Salad Wrap"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Pecans, golden raisins, ginger, leaf lettuce, whole wheat wrap"
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Roast Beef Sandwich"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Tomato bacon jam, havarti cheese, leaf lettuce, ciabatta bread"
+                    )
+                )
+            )
+        )
+    );
 }
 
 ReactDOM.render(React.createElement(Menu, null), document.getElementById('menu_container'));
--- src/food/menu.js
+++ src/food/menu.js
@@ -1,24 +1,40 @@
 
 function Menu(props) {
   return(
-  <div>
-                <h2 class="text-decoration-underline">Wine Tasting Snacks Menu</h2>
-                <dl class="row">
-                    <dt class="col-sm-3">Garden Salad</dt>
-                    <dd class="col-sm-9 text-left">Mixed greeens, tomato, cucumber, carrots, radishes, balsamic vinaigrette</dd>
-
-                    <dt class="col-sm-3">Roasted Pepper Hummus</dt>
-                    <dd class="col-sm-9 text-left">Served with pita chips and crackers</dd>
-
-                    <dt class="col-sm-3">Chimichurri Asparagus</dt>
-                    <dd class="col-sm-9 text-left">Carrots, parsley, shallot, and wine marinade</dd>
-
-                    <dt class="col-sm-3">Charcuterie Box</dt>
-                    <dd class="col-sm-9 text-left">Smoked gouda, colby jack, cured meats, pickled vegetables, hummus, pita chips or crackers</dd>
-
-                    <dt class="col-sm-3">Chicken Salad Wrap</dt>
-                    <dd class="col-sm-9 text-left">Pecans, golden raisins, ginger, leaf lettuce, whole wheat wrap</dd>
-                </dl>
+    <div>
+        <h2 class="text-decoration-underline heading">Wine Tasting Snacks Menu</h2>
+            <table class="table table-striped">
+                <tbody>
+                    <tr>
+                        <th scope="row">Feta Citrus Salad</th>
+                        <td>Mixed greens, blood orange, cucumber, goat feta cheese, sunflower seeds, citrus vinaigrette</td>
+                     </tr>
+                     <tr>
+                        <th scope="row">Stuffed Grape Leaves</th>
+                        <td>White rice, onion, mint</td>
+                    </tr>
+                                                <tr>
+                            <th scope="row">Roasted Garlic Hummus</th>
+                            <td>Served with pita chips and crackers</td>
+                        </tr>
+                                                <tr>
+                            <th scope="row">Chimichurri Asparagus</th>
+                            <td>Asparagus, carrots, red wine vinegar marinade</td>
+                        </tr>
+                                                <tr>
+                            <th scope="row">Charcuterie Box</th>
+                            <td>Smoked gouda, colby jack, cured meats, pickled vegetables, hummus, pita chips or crackers</td>
+                        </tr>
+                                                <tr>
+                            <th scope="row">Chicken Salad Wrap</th>
+                            <td>Pecans, golden raisins, ginger, leaf lettuce, whole wheat wrap</td>
+                        </tr>
+                                                <tr>
+                            <th scope="row">Roast Beef Sandwich</th>
+                            <td>Tomato bacon jam, havarti cheese, leaf lettuce, ciabatta bread</td>
+                        </tr>
+                </tbody>
+                </table>
                </div>
      )
 
