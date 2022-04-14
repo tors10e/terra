0d58dc9df60120418f9e706269e149e8635536d5
torsten <torstenaernst@gmail.com>
2022-04-14 19:35:37 -0400

    Updated food menu

--- js/react/food/menu.js
+++ js/react/food/menu.js
@@ -6,7 +6,7 @@ function Menu(props) {
         React.createElement(
             "h2",
             { "class": "text-decoration-underline heading" },
-            "Wine Tasting Snacks Menu"
+            "Terra's Food Menu"
         ),
         React.createElement(
             "table",
@@ -20,12 +20,12 @@ function Menu(props) {
                     React.createElement(
                         "th",
                         { scope: "row" },
-                        "Feta Citrus Salad"
+                        "Hummus and Pita Chips"
                     ),
                     React.createElement(
                         "td",
                         null,
-                        "Mixed greens, blood orange, cucumber, goat feta cheese, sunflower seeds, citrus vinaigrette"
+                        "Organic lemon and garlic hummus served with pita chips."
                     )
                 ),
                 React.createElement(
@@ -39,35 +39,7 @@ function Menu(props) {
                     React.createElement(
                         "td",
                         null,
-                        "White rice, onion, mint"
-                    )
-                ),
-                React.createElement(
-                    "tr",
-                    null,
-                    React.createElement(
-                        "th",
-                        { scope: "row" },
-                        "Roasted Garlic Hummus"
-                    ),
-                    React.createElement(
-                        "td",
-                        null,
-                        "Served with pita chips and crackers"
-                    )
-                ),
-                React.createElement(
-                    "tr",
-                    null,
-                    React.createElement(
-                        "th",
-                        { scope: "row" },
-                        "Chimichurri Asparagus"
-                    ),
-                    React.createElement(
-                        "td",
-                        null,
-                        "Asparagus, carrots, red wine vinegar marinade"
+                        "A blend of white rice, onion, and mint, wrapped in tender grape leaves."
                     )
                 ),
                 React.createElement(
@@ -81,7 +53,7 @@ function Menu(props) {
                     React.createElement(
                         "td",
                         null,
-                        "Smoked gouda, colby jack, cured meats, pickled vegetables, hummus, pita chips or crackers"
+                        "Cured prosciutto and salami with gorgonzola, cranberry wensleydale, and sage derby cheese, green olives, roasted almonds, dried apricots, and pita chips."
                     )
                 ),
                 React.createElement(
@@ -90,12 +62,12 @@ function Menu(props) {
                     React.createElement(
                         "th",
                         { scope: "row" },
-                        "Chicken Salad Wrap"
+                        "Flatbread Pizza"
                     ),
                     React.createElement(
                         "td",
                         null,
-                        "Pecans, golden raisins, ginger, leaf lettuce, whole wheat wrap"
+                        "Naan flatbread with your choice of pesto or marinara sauce, mozzarella and provolone cheese, topped with arugula and a drizzle of balsamic glaze."
                     )
                 ),
                 React.createElement(
@@ -104,12 +76,54 @@ function Menu(props) {
                     React.createElement(
                         "th",
                         { scope: "row" },
-                        "Roast Beef Sandwich"
+                        "Lobster Bisque"
                     ),
                     React.createElement(
                         "td",
                         null,
-                        "Tomato bacon jam, havarti cheese, leaf lettuce, ciabatta bread"
+                        "Shredded lobster blended with clam broth and sherry wine. Served with crusty french bread. *contains milk, shellfish, and wheat."
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Tiramisu"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Made from imported mascarpone and sponge cake, soaked in espresso and topped with cocoa."
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Stawberry Swirl Cheesecake"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Classic New York style cheesecake with a graham cracker crust and a strawberry swirl."
+                    )
+                ),
+                React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                        "th",
+                        { scope: "row" },
+                        "Limoncello Mascarpone"
+                    ),
+                    React.createElement(
+                        "td",
+                        null,
+                        "Lemon and limoncello liquor baked into sponge cake with mascarpone icing."
                     )
                 )
             )
--- src/food/menu.js
+++ src/food/menu.js
@@ -2,36 +2,45 @@
 function Menu(props) {
   return(
     <div>
-        <h2 class="text-decoration-underline heading">Wine Tasting Snacks Menu</h2>
+        <h2 class="text-decoration-underline heading">Terra's Food Menu</h2>
             <table class="table table-striped">
                 <tbody>
                     <tr>
-                        <th scope="row">Feta Citrus Salad</th>
-                        <td>Mixed greens, blood orange, cucumber, goat feta cheese, sunflower seeds, citrus vinaigrette</td>
+                        <th scope="row">Hummus and Pita Chips</th>
+                        <td>Organic lemon and garlic hummus served with pita chips.</td>
                      </tr>
                      <tr>
                         <th scope="row">Stuffed Grape Leaves</th>
-                        <td>White rice, onion, mint</td>
+                        <td>A blend of white rice, onion, and mint, wrapped in tender grape leaves.</td>
                     </tr>
-                                                <tr>
-                            <th scope="row">Roasted Garlic Hummus</th>
-                            <td>Served with pita chips and crackers</td>
+                    <tr>
+                        <th scope="row">Charcuterie Box</th>
+                            <td>Cured prosciutto and salami with gorgonzola, cranberry wensleydale, and
+                            sage derby cheese, green olives, roasted almonds, dried apricots, and pita
+                            chips.</td>
                         </tr>
-                                                <tr>
-                            <th scope="row">Chimichurri Asparagus</th>
-                            <td>Asparagus, carrots, red wine vinegar marinade</td>
+                        <tr>
+                            <th scope="row">Flatbread Pizza</th>
+                            <td>Naan flatbread with your choice of pesto or marinara sauce, mozzarella
+                            and provolone cheese, topped with arugula and a drizzle of balsamic glaze.</td>
                         </tr>
-                                                <tr>
-                            <th scope="row">Charcuterie Box</th>
-                            <td>Smoked gouda, colby jack, cured meats, pickled vegetables, hummus, pita chips or crackers</td>
+                        <tr>
+                            <th scope="row">Lobster Bisque</th>
+                            <td>Shredded lobster blended with clam broth and sherry wine. Served with
+                            crusty french bread. *contains milk, shellfish, and wheat.</td>
                         </tr>
-                                                <tr>
-                            <th scope="row">Chicken Salad Wrap</th>
-                            <td>Pecans, golden raisins, ginger, leaf lettuce, whole wheat wrap</td>
+                        <tr>
+                            <th scope="row">Tiramisu</th>
+                            <td>Made from imported mascarpone and sponge cake, soaked in espresso and topped with cocoa.</td>
                         </tr>
-                                                <tr>
-                            <th scope="row">Roast Beef Sandwich</th>
-                            <td>Tomato bacon jam, havarti cheese, leaf lettuce, ciabatta bread</td>
+                        <tr>
+                            <th scope="row">Stawberry Swirl Cheesecake</th>
+                            <td>Classic New York style cheesecake with a graham cracker crust and a
+                            strawberry swirl.</td>
+                        </tr>
+                         <tr>
+                            <th scope="row">Limoncello Mascarpone</th>
+                            <td>Lemon and limoncello liquor baked into sponge cake with mascarpone icing.</td>
                         </tr>
                 </tbody>
                 </table>
