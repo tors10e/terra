26f46fbb9b3355c5b65a0d9340a5330f3f0ee2b3
torsten <torstenaernst@gmail.com>
2022-09-04 09:25:36 -0400

    Added wedding page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import Navbar from "./components/navbar";
 import { BrowserRouter as Router, Routes, Route}
   from 'react-router-dom';
-import Home from './pages/home'
-import Lodging from './pages/lodging'
-import Events from './pages/events'
+import Home from './pages/home';
+import Lodging from './pages/lodging';
+import Events from './pages/events';
+import Weddings from './pages/weddings';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path='/' exact element={<Home />} />
           <Route path='/lodging' element={<Lodging />} />
           <Route path='/events' element={<Events />} />
+          <Route path='/weddings' element={<Weddings />} />
         </Routes>
       </Router>
   );
