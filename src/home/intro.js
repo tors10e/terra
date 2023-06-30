e917dd2ad6d60fe2613ea73e3630e750f17e8db9
Torsten Ernst <torsten@wostmann.com>
2023-06-29 20:54:29 -0400

    Just some small updates to the events page.

--- src/home/intro.js
+++ src/home/intro.js
@@ -7,8 +7,7 @@ const Intro = () => {
         <p className="lead">Fields of grass perched above vineyard rows surrounded
             by thousands of acres of protected forest land with unparalleled
             views of the Blueridge Mountains is the home of Terra.  Follow the meandering gravel
-            Forest Service road up backbone of Tallulah Ridge for a mile and a half,
-            leads you up the mountain and to the entrance.
+            Forest Service road up the backbone of the Tallulah Ridge for a mile and a half, to the entrance.
             Join us at our newly opened tasting room and winery, plan an event
             in our pavilion, go for a hike, or spend the night at our primitive <a href="/camping">campsite</a>.
         </p>
