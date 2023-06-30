f6d4f267ebf35341e2c1f3d51125993c6c0ce5ff
torsten <torstenaernst@gmail.com>
2023-06-29 22:56:59 -0400

    Added events for july

--- src/events/list_of_events.js
+++ src/events/list_of_events.js
@@ -1,5 +1,5 @@
 import mothers_day_poster from '../images/public_events/Brunchposter-onlineformat.jpg';
-import grand_opening from '../images/public_events/GrandOpeningFlier2023.jpg';
+import high_lonesome from '../images/public_events/lisa_philmer_high_lonesome.jpg';
 
 // Example of an event with photo  ["March 3, 2023", "First day of the season we are back to being open!", pavilion_photo]
 // Example of an event without photo  ["March 3, 2023", "First day of the season we are back to being open!"]
@@ -11,8 +11,14 @@ import grand_opening from '../images/public_events/GrandOpeningFlier2023.jpg';
             for us again! Checkout https://soundcloud.com/user-131558822 for a taste of his music. Music starts at 3pm"],
         ["May 6, 2023", "It's Lisa and Filmer Kilby time again! High Lonesome will be playing starting at 3pm."],
         ["May 14, 2023", ["Join us for a Mother's day brunch hosted at Terra! ", <a href="https://checkout.square.site/buy/KYRCKHERDJUYHAMV7WDQAKDP">Click here to buy a ticket!</a>], mothers_day_poster],
-        ["May 27, 2023", "Grand opening and open house of new tasting room and winery at 2 PM. ", grand_opening],
+//        ["May 27, 2023", "Grand opening and open house of new tasting room and winery at 2 PM. ", grand_opening],
         ["May 29, 2023", "Open for memorial day!"],
+        ["July 1, 2023", "Music by Blake Gardner playing on Saturday from 3-7pm at the Geodessy Roundhouse"],
+        ["July 4, 2023", "Open for the holiday at the Geodessy Roundhouse"],
+        ["July 8, 2023", "Brian Horton will be playing music for us from 3-7pm"],
+        ["July 15, 2023", "High Lonesome will be playing from 3-7pm", high_lonesome],
+        ["July 22, 2023", "Jake Hicks will be playing from 3-7pm", ],
+        ["July 29, 2023", "Music by George James will be playing from 3-7pm", ],
     ];
 
     export const no_events =
