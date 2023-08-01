6c97c0cff910f5c38f46e2fcc350e4ceeb5114ef
Torsten Ernst <torstenaernst@gmail.com>
2023-08-01 13:20:47 -0400

    Rolling back the hard coded pageview

--- src/home/home.js
+++ src/home/home.js
@@ -11,7 +11,7 @@ import Weather from "../home/weather";
 import ReactGA from "react-ga";
 
 const Home = () => {
-   ReactGA.pageview('home');
+   ReactGA.pageview(window.location.pathname + window.location.search);
     return (
                 <>
                 <HomeAlerts />
