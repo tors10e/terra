791ec3f9392b86a4a8d1322bea832c09f5582a48
torsten <torstenaernst@gmail.com>
2023-08-02 23:06:50 -0400

    Adding pageview test to google analytics now that it is streaming date

--- src/home/home.js
+++ src/home/home.js
@@ -8,10 +8,10 @@ import {Directions} from "../home/directions";
 import Trails from "../home/hiking";
 import Wines from "../home/wines";
 import Weather from "../home/weather";
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 const Home = () => {
-   ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send({ hitType: "pageview", page: "/home", title: "Home pageview" });
     return (
                 <>
                 <HomeAlerts />
