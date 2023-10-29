12f84b4f58e8e0fa016e45b324e2a627db214e44
Torsten Ernst <torstenaernst@gmail.com>
2023-10-28 21:37:49 -0400

    Updated payment link

---
+++ src/home/subscribe.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Subcribe = () => {
+    return (
+    <div className="topic">
+        <h1 className="cover-heading">Welcome to Terra Incognita</h1>
+        <p className="lead">Fields of grass perched above vineyard rows surrounded
+            by thousands of acres of protected forest land with unparalleled
+            views of the Blueridge Mountains is the home of Terra.  Follow the meandering gravel
+            Forest Service road up the backbone of the Tallulah Ridge for a mile and a half, to the entrance.
+            Join us at our newly opened tasting room and winery, plan an event
+            in our pavilion, go for a hike, or spend the night at our primitive <a href="/overnighting">campsite</a>.
+        </p>
+    </div>
+    );
+};
+
+export default Subscribe;
