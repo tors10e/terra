208ca9e2bb8ab91f2d5a68b4087866e8c29d09d0
torsten <torstenaernst@gmail.com>
2023-11-08 16:03:15 -0500

    Added information to the warren miller page

--- src/events/warren-miller/warren_miller_intro.js
+++ src/events/warren-miller/warren_miller_intro.js
@@ -5,12 +5,16 @@ const WarrenMillerIntro = () => {
     <div className="topic">
         <h1 className="cover-heading">Welcome to the Warren Miller Film Tour at Terra!</h1>
         <p> This is our first year hosting the Warren Miller Film Tour and we are beyond excited to bring this to
-        fellow outdoor enthusiasts in the  Southern Highlands! This is going to be great!
+        fellow outdoor enthusiasts in the  Southern Highlands, this is going to be great!
         </p>
 
         <h2 className="cover-heading">The Event</h2>
-        <p>So, what's this going to look like?</p>
-
+        <p>So, what's this going to look like? The event will be held at the Pavilion, which is located on Stoney Mountain, about  1.5 miles up a gravel Forest Service
+        Road. We are kid and dog friendly! There is a bar with wine and food available for purchase. Although the pavilion is open air, we have vinyl walls and propane heaters to keep the place
+        passably warm. That being said it's a good idea to dress for cold weather, since December can be cold and windy. We have a fire pit, for hanging around outside
+        after the show. </p>
+        <p>On the other hillside, we have a winery and tasting room which will be open from 12:30 - 9:00 pm in case you want to come early and hang out.
+        </p>
         <h2 className="cover-heading">Tickets</h2>
         <p><a href="https://checkout.square.site/buy/QKSLLJ2AGXVEACNXLEGDXMNI" target="_blank">Get your tickets here!</a></p>
 
@@ -18,10 +22,20 @@ const WarrenMillerIntro = () => {
         <p>Limited kitchen</p>
 
         <h2 className="cover-heading">Drink</h2>
-        <p>We will have wine available for sale throughout the event, but feel free to BYOB.</p>
+        <p>We will have wine, hot chocolate, coffee, tea  and probably warm Glüwein available for sale throughout the event, but feel free to BYOB.</p>
 
         <h2 className="cover-heading">Lodging</h2>
-        <p>Nearby camping</p>
+        <p><b>Camping</b> - the property is surrounded by National Forest, so camping is allowed within 100' of the pavilion.
+        Note that there are no actual campsites, but forest with flat areas where a tent can be put up. Please contact
+        us for more information on this if you are interested.</p>
+
+        <p><b>Nearby Hotels</b>:
+            <ul>
+                <li><a target="_blank" href="https://www.ihg.com/holidayinnclubvacations/hotels/us/en/clarkesville/toccv/hoteldetail">Apple Mountain Resort</a></li>
+                <li><a target="_blank" href="https://www.lakerabunhotel.com/">Lake Rabun Hotel</a></li>
+                <li><a target="_blank" href="https://glenella.com/">Glenn Ella Springs</a></li>
+            </ul>
+        </p>
     </div>
     );
 };
