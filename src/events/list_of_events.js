5156ea5d76fba9e48a1855bbcb89303a976a9465
torsten <torstenaernst@gmail.com>
2024-03-22 11:50:53 -0400

    Added easter egg hunt to events

--- src/events/list_of_events.js
+++ src/events/list_of_events.js
@@ -1,11 +1,12 @@
 import high_lonesome from '../images/public_events/lisa_philmer_high_lonesome.jpg';
-
+import easter_hunt from '../images/public_events/2024_Easter_square.JPG';
 
 // Example of an event with photo  ["March 3, 2023", "First day of the season we are back to being open!", pavilion_photo]
 // Example of an event without photo  ["March 3, 2023", "First day of the season we are back to being open!"]
+// Example of href ["December 1, 2023", ["Description of event", <a href="./events/warren-miller">Click here!</a>], some_photo],
 
   export const events = [
-        ["January 1, 2024", "Moving to winter hours, we will be open Sat-Sun 12:30 - 7pm", ],
+        ["March 30, 2024", ["Easter Egg Hunt - Kids hunt starts at 4 pm and adult hunt starts at 6 pm ", <a href='https://square.link/u/FCpcEDM1'>Click Here for Tickets!</a>], easter_hunt],
     ];
 
 
