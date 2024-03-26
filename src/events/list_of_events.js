e735304f56987d961462d8ab6f3783276a66a4c9
torsten <torstenaernst@gmail.com>
2024-03-26 13:59:09 -0400

    Added brunch and yoga to event list.

--- src/events/list_of_events.js
+++ src/events/list_of_events.js
@@ -1,12 +1,31 @@
 import high_lonesome from '../images/public_events/lisa_philmer_high_lonesome.jpg';
 import easter_hunt from '../images/public_events/2024_Easter_square.JPG';
+import yoga_paz from '../images/public_events/yoga_paz_2021.jpg';
+import mothers_day from '../images/public_events/Mothersday2024-square.jpg';
 
 // Example of an event with photo  ["March 3, 2023", "First day of the season we are back to being open!", pavilion_photo]
 // Example of an event without photo  ["March 3, 2023", "First day of the season we are back to being open!"]
 // Example of href ["December 1, 2023", ["Description of event", <a href="./events/warren-miller">Click here!</a>], some_photo],
 
-  export const events = [
-        ["March 30, 2024", ["Easter Egg Hunt - Kids hunt starts at 4 pm and adult hunt starts at 6 pm ", <a href='https://square.link/u/FCpcEDM1'>Click Here for Tickets!</a>], easter_hunt],
+  export const events = [ 
+    ["March 30, 2024", 
+          ["Easter Egg Hunt - Kids hunt starts at 4 pm and adult hunt starts at 6 pm. ", 
+            <a href='https://square.link/u/FCpcEDM1'>Click Here for Tickets!</a>
+          ], 
+      easter_hunt
+    ],
+    ["March 30, 2024",
+      ["Yoga & Wine - join Carley with Clayton Yoga at 11 AM for morning yoga flow and a free glass of wine. ", 
+      <a href='https://www.claytonyogastudio.com/events'>Buy tickets here!</a>
+      ], 
+      yoga_paz
+    ],
+    ["May 12, 2024",
+      ["Mother's day brunch 1:30 PM - 4 PM with Chef Alex Stein. ", 
+        <a href='https://checkout.square.site/merchant/K1DW0910BX83D/checkout/V2BUROP5ZQAOLAGTYNY2BPOK'>Click here to buy tickets!</a>
+      ],
+      mothers_day
+    ],  
     ];
 
 
