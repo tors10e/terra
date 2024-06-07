792ed972d7b0f1bbd951ba2cb09839ed3c38ebda
torsten <torstenaernst@gmail.com>
2024-06-07 10:28:17 -0400

    Updated banner to Josh's drone shot

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import aerial_sunset_2000 from "../images/banners/aerial_sunset_2000.jpg";
+import banner_image from "../images/banners/aerial_roundhouse_2000.jpg";
 
 
 function Banner(props) {
     return (
-        <img src={aerial_sunset_2000} alt="Aerial Sunset View"/>
+        <img src={banner_image} alt="Aerial Sunset View" class="border border-light rounded"/>
     )
 }
 
