f393339d0062ae0e5f220dfe745cb6536842aca7
Torsten <torstenaernst@gmail.com>
2025-01-15 13:51:26 -0500

    Updated css.

--- src/home/footer.js
+++ src/home/footer.js
@@ -5,26 +5,25 @@ export function Footer(props) {
     return (
         <Row>
             <Col>
-                <h5>LOCATION:</h5>
-                <p>1821 VANDIVER MTN RD</p>
-                <p>CLARKSVILLE, GA 30523</p>
+                    <h5 class="footer-heading">LOCATION:</h5>
+                    <p>1821 VANDIVER MTN RD</p>
+                    <p>CLARKSVILLE, GA 30523</p>
 
-                <p>Latitude: 34 43' 46.8588"</p>
-                <p>Longitude: -83 29' 51.6228"</p>
-
+                    <p>Latitude: 34 43' 46.8588"</p>
+                    <p>Longitude: -83 29' 51.6228"</p>
             </Col>
             <Col>
-                <h5>HOURS:</h5>
-                <p>MARCH - NOVEMBER</p>
+                <h5 class="footer-heading">HOURS:</h5>
+                <p class="font-weight-bold">MARCH - NOVEMBER</p>
                 <p>Friday, Saturday, Sunday, Monday</p>
                 <p>12:30-9:00PM</p>
 
-                <p>DECEMBER-FEBRUARY</p>
-                <p>SATURDAY, SUNDAY</p>
+                <p class="footer-subheading">DECEMBER-FEBRUARY</p>
+                <p>Saturday, Sunday</p>
                 <p>12:30-7:00PM</p>
             </Col>
             <Col>
-                <h5>CONTACT:</h5>
+                <h5 class="footer-heading">CONTACT:</h5>
                 <p>info@terraincognitavineyard.com</p>
                 <p>(828) 482-7382</p>
             </Col>
