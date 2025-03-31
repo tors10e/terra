4a59d672b117607025f174e5d17f397dee15fa5c
Torsten <torstenaernst@gmail.com>
2025-03-31 15:38:09 -0400

    Fixed some dependency errors

---
+++ src/weddings/contact_form.js
@@ -0,0 +1,46 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function ContactForm() {
+  return (
+    <Form>
+        <Form.Group className="mb-3" controlId="formFirstName">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="name_first" placeholder="First Name *" />
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formLastName">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="name_last" placeholder="Last Name *" />
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formPhone">
+        <Form.Label>Phone Number</Form.Label>
+        <Form.Control type="phone" placeholder="Phone Number" />
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+            </Form.Text>
+      </Form.Group>
+
+       <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default ContactForm;
