67bfa28b561165648a343645b54cabc0fce84136
Torsten Ernst <torstenaernst@gmail.com>
2025-06-03 08:03:15 -0400

    Added lightbox to gallery

--- src/gallery/PhotoGalleryTest.js
+++ src/gallery/PhotoGalleryTest.js
@@ -1,13 +1,26 @@
 import PhotoAlbum from "react-photo-album";
-import "react-photo-album/rows.css";
+import "react-photo-album/styles.css";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
+// import optional lightbox plugins
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+import React, { useState } from 'react';
 
 const photos = [
  
   {
     "src": "images/gallery/079A1642.jpg",
-        "width": 800,
-    "height": 600
+    "width": 1600,
+    "height": 1200,
+    "title": "A beautiful sunset over the mountains",
+    "alt": "Sunset over mountains",
+    "label": "Sunset",
   },
   {
     "src": "images/gallery/079A1646.jpg",
@@ -16,12 +29,12 @@ const photos = [
   },
   {
     "src": "images/gallery/079A1976.jpg",
-    "width": 800,
-    "height": 600
+    "width": 1200,
+    "height": 800
   },
   {
     "src": "images/gallery/079A2002.jpg",
-    "width": 800,
+    "width": 1200,
     "height": 600
   },
   {
@@ -97,16 +110,28 @@ const photos = [
   }
 ];
 
-export function PhotoGallery() {
-    return (
-<div>
-        <h1>photo gallery</h1>
-        <PhotoAlbum layout="rows" photos={photos} 
-         spacing={20}
-        targetRowHeight={200}
-        rowConstraints={(containerWidth) => ({ maxPhotos: containerWidth < 600 ? 1 : 4 })}
-      />
-     </div>
-    )
-}
 
+export function ColumnsPhotoAlbum() {
+    const [index, setIndex] = useState(-1);
+
+    return (
+        <>        
+        <h1>photo gallery</h1>
+            <PhotoAlbum 
+            layout="columns"
+            photos={photos} 
+            spacing={20}
+            targetRowHeight={300}
+            onClick={({ index }) => setIndex(index)}
+            // rowConstraints={(containerWidth) => ({ maxPhotos: containerWidth < 600 ? 1 : 4 })}
+        />
+        <Lightbox
+            index={index}
+            slides={photos}
+            open={index >= 0}
+            close={() => setIndex(-1)}
+            plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+            />
+        </>
+        );
+}
