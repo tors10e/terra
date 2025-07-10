bfbff1fddaa5778f998ddd3a4d648e9095624bf7
Torsten Ernst <torstenaernst@gmail.com>
2025-07-09 21:37:25 -0400

    updated image to picture of the tasting room

--- src/winery/banner.js
+++ src/winery/banner.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container, Image } from 'react-bootstrap';
+import banner_image from "../images/banners/tasting_room_2000x1000.jpg";
+
 
 export function Banner(props) {
     return (
         <Container id="banner">
-            <Image src="/images/banners/snowy_vines.jpg" fluid className="bg-image d-flex justify-content-center align-items-center" />
+            <Image src={banner_image} fluid className="bg-image d-flex justify-content-center align-items-center" />
         </Container>
     )
 }
