199cf3835fcc2016d874e79333acb34cd5a5a550
Torsten Ernst <torstenaernst@gmail.com>
2025-10-15 12:28:46 -0400

    Added wedding pricing pamphlet image to pricing page

--- src/weddings/wedding_pricing.js
+++ src/weddings/wedding_pricing.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Image from 'react-bootstrap/Image';
 import Wedding_Banner from "./wedding_pricing_banner";
 import Header from '../components/header';
 import NavBar from "../components/navbar";
-import ContactForm from './wedding_contact_form';
 import ReactGA from "react-ga4";
 import { Container, Row, Col } from 'react-bootstrap';
 import { WeddingMenu } from './wedding_menu';
 import WeddingsIframeComponent from '../components/weddings/wedding_iframe';
-
-
+import WeddingPricingPamphlet from "../images/wedding_pricing_pamphlet.png";
+import CeremonyRock from "../images/event_photos/ceremony_rock.jpg";
 
 const Weddings = () => {
     ReactGA.send({ hitType: "pageview", page: "/weddings", title: "Weddings" });
@@ -23,12 +23,10 @@ const Weddings = () => {
                 <Row>
                     <Wedding_Banner/>
                 </Row>
+                <Row></Row>
                 <Row>
-                    <WeddingsIframeComponent/>
-                </Row>
-                <Row>
-                    <Col></Col>
-                    <Col></Col>
+                    <Col> <Image src={WeddingPricingPamphlet}/> </Col>
+                    <Col><WeddingsIframeComponent/></Col>
                     <Col></Col>
                 </Row>
             </Container>
